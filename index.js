ecee73652dff5e9160d1800e794ba743bb834729
Devarshi <devarshidwi@gmail.com>
2024-01-18 16:07:11 +0530

    Fix

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
-import axios from 'axios';
 import express from 'express';
-
+import axios from 'axios';
 const app = express();
+
 const PORT = 5000;
 
-const base_url = 'https://mern-todo-backend-vercel.vercel.app/api/v1'; // Replace with your actual base URL
+const warmUpInterval = 1000 * 60 * 5;
 
-const getEndpoints = ['/task/my', '/users/me'];
+const mainBackendUrl = 'https://mern-todo-backend-vercel.vercel.app/api/v1';
 
-const postEndpoints = ['/users/login'];
+const endpointsToWarmGET = ['/task/my', '/users/me'];
 
-const warmUpInterval = 1000 * 60 * 15;
+const endpointsToWarmPOST = ['/users/login'];
 
-const warmServer = () => {
-	getEndpoints.forEach((endpoint) => {
-		const url = base_url + endpoint;
+async function keepEndpointWarmGET(endpoint) {
+	try {
+		await axios.get(`${mainBackendUrl}${endpoint}`);
+		console.log(`Warming call successful for ${endpoint}`);
+	} catch (error) {
+		console.error(`Error warming ${endpoint}: ${error.response.data.message}`);
+	}
+}
 
-		axios
-			.get(url)
-			.then((response) => {
-				console.log(`Request to ${url} sent successfully`);
-			})
-			.catch((error) => {
-				console.error(
-					`Error sending request to ${url}:`,
-					error.response.data.message
-				);
-			});
-	});
+async function keepEndpointWarmPOST(endpoint) {
+	try {
+		await axios.post(`${mainBackendUrl}${endpoint}`);
+		console.log(`Warming call successful for ${endpoint}`);
+	} catch (error) {
+		console.error(`Error warming ${endpoint}: ${error.response.data.message}`);
+	}
+}
 
-	postEndpoints.forEach((endpoint) => {
-		const url = base_url + endpoint;
+setInterval(async () => {
+	for (const endpoint of endpointsToWarmGET) {
+		await keepEndpointWarmGET(endpoint);
+	}
 
-		axios
-			.post(url)
-			.then((response) => {
-				console.log(`Request to ${url} sent successfully`);
-			})
-			.catch((error) => {
-				console.error(
-					`Error sending request to ${url}:`,
-					error.response.data.message
-				);
-			});
-	});
-};
-
-setInterval(warmServer, warmUpInterval);
+	for (const endpoint of endpointsToWarmPOST) {
+		await keepEndpointWarmPOST(endpoint);
+	}
+}, warmUpInterval);
 
 app.listen(PORT, () => {
-	console.log(`Listening on PORT ${PORT}`);
+	console.log(`Express server listening on port ${PORT}`);
 });
